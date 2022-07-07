930b6e8a42436d80eec49a133e3f906b180070db
Sam Gaus <sam@gaus.co.uk>
2022-07-07 14:04:00 +0100

    Update on poke

--- Casks/kolmafia.rb
+++ Casks/kolmafia.rb
@@ -2,7 +2,7 @@ cask "kolmafia" do
   version "26524"
   sha256 "afa1d1d41edd0891091676d1bda3ed5784b0f6d734d377945683a27b70d8c78d"
 
-  url "https://github.com/kolmafia/kolmafia/releases/download/r#{version}/Kolmafia-22.07.#{version}.dmg",
+  url "https://github.com/kolmafia/kolmafia/releases/download/r26524/Kolmafia-22.07.26524.dmg",
       verified: "github.com/kolmafia/kolmafia"
   name "KoLmafia"
   desc "Cross-platform application to interface with online RPG Kingdom of Loathing 🍸"
---
+++ bump.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+RELEASE_INFO=$(curl -s -H "Accept: application/vnd.github+json" https://api.github.com/repos/kolmafia/kolmafia/releases/latest)
+          
+VERSION=$(echo $RELEASE_INFO | jq -r '.name')
+URL=$(echo $RELEASE_INFO | jq -r '.assets | map(select(.name | endswith("dmg"))) | .[0].browser_download_url')
+SHA=($(curl -s ${URL} | shasum -a 256))
+
+sed -i'.backup' -e "s;version \"[0-9]*\";version \"${VERSION}\";" -e "s;sha256 \"[a-f0-9]*\";sha256 \"${SHA}\";" -e "s;url \".*dmg\";url \"${URL}\";" Casks/kolmafia.rb
